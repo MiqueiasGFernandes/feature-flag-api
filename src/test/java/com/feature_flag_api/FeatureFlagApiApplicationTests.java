78026111d8981d8c3dc39fb456776ebd320e9202
miqueias.fernandes@bitfy.app <miqueiasgomesfernandes@gmail.com>
2026-06-20 21:54:53 -0300

    chore: replace struct by maven pattern

--- src/test/java/com/feature_flag_api/FeatureFlagApiApplicationTests.java
+++ src/test/java/com/feature_flag_api/FeatureFlagApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.feature_flag_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class FeatureFlagApiApplicationTests {
 
 	@Test
 	void contextLoads() {
